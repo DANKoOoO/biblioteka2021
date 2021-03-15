6fc0315a8646295f92e88f2e6c0f81d23ed303e6
DANKoOoO <danko.97@hotmail.com>
2021-03-15 15:39:41 +0100

    Dokumentovana klasa Autor

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -1,14 +1,42 @@
 package biblioteka;
 
+/**
+ * Klasa koja predstavlja autora knjige
+ * 
+ * Autor ima ime i prezime kao string vrednosti.
+ * 
+ * @author danko
+ * @version 0.1
+ *
+ */
+
 public class Autor {
-
+	/**
+	 * Ime autora kao String.
+	 */
     private String ime;
+    
+    /**
+     * Prezime autora kao String.
+     */
     private String prezime;
-
+    
+    /**
+     * Vraca ime autora.
+     * @return vraca ime autora kao String
+     */
     public String getIme() {
         return ime;
     }
-
+    
+    /**
+     * Postavlja ime autora na novu vrednost.
+     * 
+     * @param ime ime autora koa String
+     * 
+     * @throws java.lang.NullPointerException ako je uneto ime null
+     * @throws java.lang.RuntimeException ako je uneto ime koje ima manje od 2 znaka
+     */
     public void setIme(String ime) {
         if(ime == null)
             throw new NullPointerException("Ime ne sme biti null");
@@ -17,11 +45,22 @@ public class Autor {
 
         this.ime = ime;
     }
-
+    /**
+     * Vraca prezime autora.
+     * @return vraca prezimeime autora kao String
+     */
     public String getPrezime() {
         return prezime;
     }
 
+    /**
+     * Postavlja prezime autora na novu vrednost.
+     * 
+     * @param prezime prezime autora koa String
+     * 
+     * @throws java.lang.NullPointerException ako je uneto prezime null
+     * @throws java.lang.RuntimeException ako je uneto prezime koje ima manje od 2 znaka
+     */
     public void setPrezime(String prezime) {
         if(prezime == null)
             throw new NullPointerException("prezime ne sme biti null");
@@ -30,16 +69,26 @@ public class Autor {
         this.prezime = prezime;
     }
 
+    /**
+     * Konstruktor koji inicijalizuje objekat i nista vise.
+     */
     public Autor() {
         super();
     }
-
+    /**
+     * Konstruktor koji inicijalizuje objekat i postavlja vrednosti za ime i prezime autora.
+     * 
+     * @param ime Ime autora kao String
+     * @param prezime Prezime autora kao String
+     */
     public Autor(String ime, String prezime) {
         super();
         setIme(ime);
         setPrezime(prezime);
     }
-
+    /**
+     * @return String sa svim podacima o Autoru
+     */
     @Override
     public String toString() {
         return "Autor [ime=" + ime + ", prezime=" + prezime + "]";
@@ -53,7 +102,17 @@ public class Autor {
         result = prime * result + ((prezime == null) ? 0 : prezime.hashCode());
         return result;
     }
-
+    
+    /**
+     * Poredi 2 autora i vraca true ako su isti a false ako nisu.
+     * Autori se porede po imenu i prezimenu i oba moraju da budu ista.
+     * 
+     * @return 
+     * <ul>
+     * 		<li>true ako su oba objekta klase Autor i imaju ista imena i prezimena</li>
+     * 		<li>false u svim ostalim slucajevima</li>
+     * </ul>
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
