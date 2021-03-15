b8b78909e3ce0474feaea37b33368bd0f9fdc0d5
DANKoOoO <danko.97@hotmail.com>
2021-03-15 15:43:52 +0100

    Dokumentovan interfejs BibliotekaInterfejs

--- src/biblioteka/interfejs/Bibliotekainterfejs.java
+++ src/biblioteka/interfejs/Bibliotekainterfejs.java
@@ -4,8 +4,20 @@ import java.util.LinkedList;
 
 import biblioteka.Autor;
 import biblioteka.Knjiga;
-
+/**
+ * Interfejs koji predstavlja biblioteku.
+ * 
+ * @author danko
+ *
+ */
 public interface Bibliotekainterfejs {
+	/**
+	 * Dodaje novu knjigu u biblioteku.
+	 * @param knjiga Knjiga koja se dodaje.
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneta knjiga null.
+	 * @throws java.lang.RuntimeException Ako uneta knjiga vec postoji u biblioteci (duplikat)
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
 	public void obrisiKnjigu(Knjiga knjiga);
 	public LinkedList<Knjiga> vratiSveKnjige();
