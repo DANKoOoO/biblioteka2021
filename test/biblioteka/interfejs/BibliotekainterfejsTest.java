2a24ac125b1827014e8b3ee9f071284fa28cea71
DANKoOoO <danko.97@hotmail.com>
2021-03-15 14:47:36 +0100

    Testiran interfejs BibliotekaInterfejs

--- test/biblioteka/interfejs/BibliotekainterfejsTest.java
+++ test/biblioteka/interfejs/BibliotekainterfejsTest.java
@@ -2,6 +2,8 @@ package biblioteka.interfejs;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import biblioteka.Knjiga;
@@ -14,21 +16,110 @@ public abstract class BibliotekainterfejsTest {
 	void testDodajKnjigu() {
 		Knjiga k = new Knjiga();
 		k.setNaslov("Knjiga 1");
+		
+		biblioteka.dodajKnjigu(k);
+	
+		assertEquals(1, biblioteka.vratiSveKnjige().size());	
+		assertEquals(k, biblioteka.vratiSveKnjige().get(0));
 	}
 
 	@Test
+	void testDodajKnjiguNull() {
+		assertThrows(java.lang.NullPointerException.class,
+				() -> biblioteka.dodajKnjigu(null) );
+	}
+
+	@Test
+	void testDodajKnjiguDuplikat() {
+		Knjiga k = new Knjiga();
+		k.setIsbn("12345");
+		Knjiga k2 = new Knjiga();
+		k2.setIsbn("12345");
+		
+		biblioteka.dodajKnjigu(k);
+		
+		RuntimeException e = assertThrows(java.lang.RuntimeException.class,
+				() -> biblioteka.dodajKnjigu(k2) );
+		assertEquals("Knjiga vec postoji", e.getMessage() );
+	}
+
+	
+	@Test
 	void testObrisiKnjigu() {
-		fail("Not yet implemented");
+		Knjiga k = new Knjiga();
+		k.setIsbn("12345");
+		
+		biblioteka.dodajKnjigu(k);
+		biblioteka.obrisiKnjigu(k);
+		
+		assertEquals(0, biblioteka.vratiSveKnjige().size());
+		
+	}
+	
+	@Test
+	void testObrisiKnjiguNull() {
+		assertThrows(java.lang.RuntimeException.class,
+				() -> biblioteka.obrisiKnjigu(null));
+		
+	}
+
+	
+	@Test
+	void testObrisiKnjiguNePostoji() {
+		Knjiga k = new Knjiga();
+		k.setIsbn("12345");
+		Knjiga k2 = new Knjiga();
+		k2.setIsbn("34242342");
+		
+		assertThrows(java.lang.RuntimeException.class,
+				() -> biblioteka.obrisiKnjigu(k2));
+		
 	}
 
 	@Test
 	void testVratiSveKnjige() {
-		fail("Not yet implemented");
+		Knjiga k = new Knjiga();
+		k.setIsbn("12345");
+		
+		biblioteka.dodajKnjigu(k);
+		
+		List<Knjiga> knjige = biblioteka.vratiSveKnjige();
+		
+		assertEquals(1, knjige.size());
+		assertEquals(k, knjige.get(0));
 	}
 
 	@Test
-	void testPronadjiKnjigu() {
-		fail("Not yet implemented");
+	void testPronadjiKnjiguNull() {
+		assertThrows(java.lang.RuntimeException.class,
+				() -> biblioteka.pronadjiKnjigu(null, null, null, null));
+		
 	}
+	
+	@Test
+	void testPronadjiKnjigu() {
+		Knjiga k1 = new Knjiga();
+		k1.setIsbn("1234");
+		k1.setNaslov("Knjiga o dzungli");
+		
+		Knjiga k2 = new Knjiga();
+		k2.setIsbn("5678");
+		k2.setNaslov("Dzungla zivota");
+		
+		Knjiga k3 = new Knjiga();
+		k3.setNaslov("Knjiga 3");
+		k3.setIsbn("91011");
+		
+		biblioteka.dodajKnjigu(k1);
+		biblioteka.dodajKnjigu(k2);
+		biblioteka.dodajKnjigu(k3);
+		
+		List<Knjiga> rezultat = biblioteka.pronadjiKnjigu(null, null, null,"zung");
+		
+		assertEquals(2, rezultat.size());
+		assertTrue(rezultat.contains(k1));
+		assertTrue(rezultat.contains(k2));
+	}
+
 
 }
